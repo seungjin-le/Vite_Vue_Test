5c7b40029e79263799621294e8fb969a083c1cf7
Lee Seung Jin <dltmdwls154@gmail.com>
2024-03-02 22:15:04 +0900

    First Commit

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+const moduls = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  bracketSameLine: true,
+  embeddedLanguageFormatting: 'auto',
+  endOfLine: 'lf',
+  jsxBracketSameLine: false,
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+};
+
+export default moduls;
